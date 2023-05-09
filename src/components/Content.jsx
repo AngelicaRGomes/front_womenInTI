3ebdc6a582947e6ba87967d53d35652fe9201de3
AngelicaRGomes <132945489+AngelicaRGomes@users.noreply.github.com>
2023-05-08 22:35:41 -0300

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -14,7 +14,7 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'https://womeninti.onrender.com'
+  const baseURL = 'https://womeninti.onrender.com/women'
 
   useEffect(() => {
     async function getData() {
