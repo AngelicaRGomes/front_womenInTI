0f39b72ced4d1ac6ed73f0d442e91a09296fbbe1
AngelicaRGomes <132945489+AngelicaRGomes@users.noreply.github.com>
2023-05-09 00:17:40 -0300

    removing dot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/footer.module.css'
 export function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}>2023.</p>
+      <p className={styles.footerText}>2023</p>
     </div>
   )
 }
